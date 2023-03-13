f7d529c382a2c1da3c9ae484a9533c72a36c56e5
Boris <rasskazovboris@mail.ru>
2023-03-13 22:00:11 +0400

    addFirstFunction

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите длину массива: ");
+int numberOfDigits = int.Parse(Console.ReadLine());
+int numOfLinesLessThen3 = 0;
+string[] mainArray(int size)
+{
+    string data;
+    string[] firstStrings = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите значение элемента №{i} в массиве: ");
+        data = Console.ReadLine();
+        firstStrings[i] = data;
+        numOfLinesLessThen3 = data.Length <= 3 ? numOfLinesLessThen3 + 1 : numOfLinesLessThen3 + 0;
+    }
+    return firstStrings;
+}
